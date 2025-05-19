ed396319335502e363d1fc250bacfc47558e4f95
Sam Numan <snum.dev@gmail.com>
2025-05-19 03:45:06 -0700

    2.0: scraping authors and quotes inside quote blocks

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Step 1: Get the HTML content of the page
+# get the HTML content of the page
 url = 'http://quotes.toscrape.com'
 response = requests.get(url)
 html = response.text
 
-# Step 2: Parse the HTML using BeautifulSoup
+# parse the HTML using BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
-# Step 3: Find and print all quotes
-quotes = soup.find_all('span', class_='text')
-for quote in quotes:
-    print(quote.text)
+# find all quote blocks
+blocks = soup.find_all('div', class_='quote')
+
+# find quotes and authors in blocks
+for block in blocks:
+    quote = block.find('span', class_='text')
+    author = block.find('small', class_='author')
+    print(quote.text + ' - ' + author.text)
